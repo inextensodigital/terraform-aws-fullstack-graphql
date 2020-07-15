c090b9888e85c5b9ccafa3515e76a6c56d7494a7
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2020-07-15 15:07:58 +0200

    use new version of graphql lambda

--- main.tf
+++ main.tf
@@ -3,10 +3,8 @@ terraform {
 }
 
 module "ied-lambda-graphql" {
-  # source  = "app.terraform.io/ied/lambda-graphql/aws"
-  # version = "~>2.0.0"
-
-  source = "../terraform-aws-lambda-graphql"
+  source  = "app.terraform.io/ied/lambda-graphql/aws"
+  version = "~>2.0.1"
 
   providers = {
     aws            = "aws"
